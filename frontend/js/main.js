bb21299ea0e40e5425cdb8be41e58568e49093ab
mpmckenna8 <mpmckenna8@me.com>
2020-08-02 13:07:50 -0700

    Separated out the code from all being in the html file and made it load all the items in the itemizer database.

---
+++ frontend/js/main.js
@@ -0,0 +1,50 @@
+// a js file to be the js entrypoint for this app.
+ // define the callAPI function that takes a first name and last name as parameters
+ var getAllItems = ()=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    //var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    // make API call with parameters and use promises to get response
+    fetch("https://j2my5r4vr7.execute-api.us-west-2.amazonaws.com/dev/items", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        console.log(JSON.parse(result).body)
+        let allItems = JSON.parse(JSON.parse(result).body)
+        console.log("allItems =", allItems)
+        makeItemsTable(allItems)
+       // alert(JSON.parse(result).body)
+    })
+    .catch(error => console.log('error with the request to get all Items', error));
+}
+
+
+function makeItemsTable(items) {
+    let itemsTable = document.querySelector('.item_table');
+
+    let numberOfItems = items.length;
+
+    for( let i = 0; i < numberOfItems; i++) {
+
+        let row = itemsTable.insertRow( i + 1 )
+        // putting a cell in the new row
+        let cell = row.insertCell( 0 )
+        cell.innerHTML = items[i].title
+
+    }
+
+}
+
+window.onload = function() {
+    getAllItems()
+
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
 Another go at creating an inventory app. Maybe this one will be semi functional!
+
+basic walkthrough of tools I'm using:
+https://aws.amazon.com/getting-started/hands-on/build-web-app-s3-lambda-api-gateway-dynamodb/?e=gs2020&p=fullstack
+
+
+
+tooling:
+Looking to use AWS for hosting with github to keep track of lots of my frontend code which probably makes it a lot easier to hack but whatevs. no one uses it but me.
+
+AWS Amplify is connected to my github repo and updates as that updates.
+
+
+Using API gateway to handle requests from the frontend client.
+- Need to make sure looking at the US-west-2 region to find the one I'm using.
+
+
+
+
+
+Node Tooling:
+
+Trying to use much more modern node version than usual.  Using n to handle node versions. https://github.com/tj/n
+
+To do local hosting using http-server
